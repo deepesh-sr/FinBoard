ca9da6b6260551feda9a6fb85630e8d62e56f01c
deepesh-sr <rathoredeepeshsingh@gmail.com>
2026-02-04 21:24:10 +0530

    Add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  fetch('http://localhost:5001/api/stocks/tech-sector')
+  .then(res => res.json())
+  .then(data => console.log(data));
   return (
     <>
       <div>
         <p className='text-5xl'>Deepesh</p>
+        
        </div>
     </>
   )
