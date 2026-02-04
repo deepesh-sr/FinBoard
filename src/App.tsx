f347a1d804e0aad1ee37f5d3d006e1a83c11fd7d
deepesh-sr <220714100078@cutm.ac.in>
2026-02-04 21:37:55 +0530

    Add shadcn table

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
 import { useState } from 'react'
+import { Button } from './components/ui/button';
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 function App() {
   const [count, setCount] = useState(0);
-  fetch('http://localhost:5001/api/stocks/tech-sector')
-  .then(res => res.json())
-  .then(data => console.log(data));
   return (
     <>
-      <div>
-        <p className='text-5xl'>Deepesh</p>
-        
-       </div>
+      <div className='h-screen bg-gray-900'>
+        <div className='h-1/2 w-1/2 text-white'>
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="w-[100px]">Invoice</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Method</TableHead>
+              <TableHead className="text-right">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">INV001</TableCell>
+              <TableCell>Paid</TableCell>
+              <TableCell>Credit Card</TableCell>
+              <TableCell className="text-right">$250.00</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+        </div>
+      </div>
     </>
   )
 }
